59fb402dff3135fb17df71feb0de2e6b0052b58f
jatinmandav3@gmail.com <jatinmandav3@gmail.com>
2019-12-02 18:36:05 +0530

    improved tuple extractor

--- tuple_extractor.py
+++ tuple_extractor.py
@@ -14,19 +14,53 @@ from nltk.stem import WordNetLemmatizer, PorterStemmer
 class ExtractTuple:
     def __init__(self):
         self.accepted_puntuations = ['-']
-        self.rejected_words_ingredients = ['cup', 'tablespoon', 'teaspoon', 'tablespoons']
+        self.rejected_words_ingredients = ['cup', 'tablespoon', 'teaspoon']
+
+        self.action_words = ['break', 'melt', 'spread', 'layer', 'roll out',
+                            'fry', 'peel', 'mix', 'whip', 'saute', 'taste',
+                            'cut', 'chop', 'slice', 'grate', 'boil', 'steam',
+                            'pinch', 'pour', 'add', 'barbeque', 'roast', 'bake',
+                            'stir', 'weigh', 'whisk', 'combine', 'fold', 'fill',
+                            'heat']
+
+
         self.lemmatizer = WordNetLemmatizer()
         self.stemmer = PorterStemmer()
+
+    def substring_matching(self, str1, str2):
+        str1 = [s for s in str1.split(' ') if s != '']
+        str2 = [s for s in str2.split(' ') if s != '']
+
+        i = 0
+        j = 0
+        count = 0
+        while i < len(str1) and j < len(str2):
+            if str1[i] == str2[j]:
+                i += 1
+                j += 1
+                count += 1
+            else:
+                i += 1
+
+        if count >= len(str1)*0.2:
+            return True
+
+        return False
+
+    def is_valid_ingredient(self, ingredients, ingredient):
+        for ing in ingredients:
+            if self.substring_matching(ingredient, ing):
+                return True
+            if self.substring_matching(ing, ingredient):
+                return True
+        return False
 
     def pos_tag(self, text):
         return nltk.pos_tag(text)
 
     def extract_ingredient(self, text):
-        # ner = self.ner_tagger.ner_tag(text)
         text = [char for char in text.split(' ') if char != '']
         tags = self.pos_tag(text)
-        print()
-        print(tags)
 
         i = 0
         ingredient = []
@@ -35,16 +69,19 @@ class ExtractTuple:
         while i < len(tags):
             if tags[i][1].startswith('NN'):
                 continoues = True
-                if tags[i][0] not in self.rejected_words_ingredients:
-                    ingredient.append(self.lemmatizer.lemmatize(tags[i][0]))
-            # elif tags[i][1] == 'JJ' or tags[i][1] == 'VBP' or tags[i][1] == 'VBN':
-            #     continoues = True
-            #     if tags[i][0] not in self.rejected_words_ingredients:
-            #         ingredient.append(self.lemmatizer.lemmatize(tags[i][0]))
+                word = self.lemmatizer.lemmatize(tags[i][0])
+                if word not in self.rejected_words_ingredients:
+                    ingredient.append(self.lemmatizer.lemmatize(word))
+            if tags[i][1] == 'JJ' or tags[i][1] == 'VBD' or tags[i][1] == 'VBP':
+                continoues = True
+                word = self.lemmatizer.lemmatize(tags[i][0])
+                if word not in self.rejected_words_ingredients:
+                    ingredient.append(self.lemmatizer.lemmatize(word))
             elif tags[i][0] in self.accepted_puntuations and continoues:
                 continoues = True
-                if tags[i][0] not in self.rejected_words_ingredients:
-                    ingredient.append(self.lemmatizer.lemmatize(tags[i][0]))
+                word = self.lemmatizer.lemmatize(tags[i][0])
+                if word not in self.rejected_words_ingredients:
+                    ingredient.append(self.lemmatizer.lemmatize(word))
             else:
                 continoues = False
                 if ingredient != []:
@@ -68,20 +105,26 @@ class ExtractTuple:
         text = [char for char in text.split(' ') if char != '']
         tags = self.pos_tag(text)
 
-        print(tags)
+        # print(tags)
 
         action = None
         i = 0
         ingredient = []
         continoues = False
         while i < len(tags):
+            # if tags[i][1] == 'NNP' or tags[i][1].startswith('VB'):
+            #     if tags[i][0].lower() in self.action_words:
+            #         action = tags[i][0].lower()
+
             if tags[i][1] == 'NNP':
-                action = tags[i][0]
+                if tags[i][0].lower() in self.action_words:
+                    action = tags[i][0].lower()
                 continoues = False
                 if ingredient != []:
-                    ingredient = ' '.join(ingredient)
+                    ingredient = ' '.join([self.lemmatizer.lemmatize(word) for word in ingredient])
                     if action != None:
-                        tuples.append([action, ingredient])
+                        if self.is_valid_ingredient(ingredients, ingredient):
+                            tuples.append([action, ingredient])
                     ingredient = []
             elif tags[i][1] == 'JJ' or tags[i][1] == 'VBD':
                 continoues = True
@@ -90,11 +133,16 @@ class ExtractTuple:
                 continoues = True
                 ingredient.append(tags[i][0])
             elif tags[i][1].startswith('VB'):
+                if tags[i][0].lower() in self.action_words:
+                    # print(tags[i][0])
+                    action = tags[i][0].lower()
                 continoues = False
                 if ingredient != []:
-                    ingredient = ' '.join(ingredient)
+                    ingredient = ' '.join([self.lemmatizer.lemmatize(word) for word in ingredient])
                     if action != None:
-                        tuples.append([action, ingredient])
+                        # print(self.is_valid_ingredient(ingredients, ingredient), ingredient)
+                        if self.is_valid_ingredient(ingredients, ingredient):
+                            tuples.append([action, ingredient])
                     ingredient = []
             elif tags[i][0] in self.accepted_puntuations and continoues:
                 continoues = True
@@ -102,39 +150,53 @@ class ExtractTuple:
             else:
                 continoues = False
                 if ingredient != []:
-                    ingredient = ' '.join(ingredient)
+                    ingredient = ' '.join([self.lemmatizer.lemmatize(word) for word in ingredient])
                     if action != None:
-                        tuples.append([action, ingredient])
+                        if self.is_valid_ingredient(ingredients, ingredient):
+                            tuples.append([action, ingredient])
                     ingredient = []
 
-            # print(tags[i], ingredient)
+            # print(action, ingredient, tags[i])
             i += 1
+
         return tuples
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', help='Path to cleaned dataset', default='dataset/processed_recipes.tsv', type=str)
+parser.add_argument('--num_recipes', '-n', help='Number of Recipes to process', default=10, type=int)
 args = parser.parse_args()
 
 df = pd.read_csv(args.file, sep='\t', lineterminator='\n')
 
-index = 1
-title = df.title[index]
-ingredients = df.ingredients[index].split('|')
-ingredients = [ing for ing in ingredients if 'Add' not in ing]
-directions = df.directions[index]
+writer = open('extracted_tuples.txt', 'w')
 
-extractor = ExtractTuple()
+for index in range(args.num_recipes):
+    title = df.title[index]
+    ingredients = df.ingredients[index].split('|')
+    ingredients = [ing for ing in ingredients if 'Add' not in ing]
+    directions = df.directions[index]
 
-for i in range(len(ingredients)):
-    tuples = '|'.join(extractor.extract_ingredient(ingredients[i]))
-    print(tuples)
-    ingredients[i] = tuples
+    extractor = ExtractTuple()
 
-print(directions)
+    for i in range(len(ingredients)):
+        tuples = ' '.join(extractor.extract_ingredient(ingredients[i]))
+        ingredients[i] = tuples
+    ingredients = [ing for ing in ingredients if ing != '']
+    # print(ingredients)
 
-print()
-tuples = extractor.extract(directions, ' '.join(ingredients))
+    tuples = extractor.extract(directions, ingredients)
+    writer.write('\n*** RECIPE ***\n')
+    writer.write('INGREDIENTS: {}\n\n'.format(', '.join(ingredients)))
+    writer.write('DIRECTIONS: {}\n\n'.format(directions))
 
-for tup in tuples:
-    print(tup)
+    tuple_formatted = []
+
+    for tup in tuples:
+        tuple_formatted.append('({}, {})'.format(tup[0], tup[1]))
+
+    writer.write('TUPLES: {}\n'.format(' -> '.join(tuple_formatted)))
+
+    print('{} of {} processed.'.format(index+1, args.num_recipes))
+
+writer.close()
